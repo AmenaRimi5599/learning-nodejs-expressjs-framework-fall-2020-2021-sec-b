228d41ba3b547eb4782e701edc0911a00ff329fd
AmenaRimi <67853864+AmenaRimi5599@users.noreply.github.com>
2020-11-28 00:04:54 +0600

    add comment model

--- Mid_assignment/TravelGuide/models/commentModel.js
+++ Mid_assignment/TravelGuide/models/commentModel.js
@@ -1,7 +1,7 @@
 const db = require('./db');
 module.exports ={
 insertComment: function(user, callback){
-		var sql = "INSERT INTO `comment` VALUES ('','"+user.touristspotsId+"' , '"+user.comment+"' ,  '"+user.commenterId+"'";
+		var sql = "INSERT INTO comment VALUES ('','"+user.touristspotsId+"' , '"+user.comment+"' ,  '"+user.commenterId+"'";
 		console.log(sql);
 
 		db.execute(sql, function(status){
