98fa9425798d83b24de428a8b3620b526eedcb61
Rimjhim <amenarimi5@gmail.com>
2020-12-23 12:07:16 +0600

    first commit

---
+++ Final_lab_xm/resources/views/home/create.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Home page</title>
+</head>
+<body>
+	<a href="{{route('home.index')}}">Back</a> |
+	<a href="/logout">logout</a>
+	<br>
+
+
+		<form method="post" enctype="multipart/form-data">
+
+			<input type="hidden" name="_token" value="{{csrf_token()}}">
+			<fieldset>
+				<legend>Create User</legend>
+			<table>
+				<tr>
+					<td>Username</td>
+					<td><input type="text" name="username" value="{{old('username')}}"></td>
+				</tr>
+				<tr>
+					<td>Password</td>
+					<td><input type="password" name="password" value="{{old('password')}}"></td>
+				</tr>
+				<tr>
+					<td>type</td>
+					<td><input type="text" name="type" value="{{old('type')}}"></td>
+				</tr>
+				<tr>
+					<td>Photo</td>
+					<td><input type="file" name="myimg"></td>
+				</tr>
+				<tr>
+					<td></td>
+					<td><input type="submit" name="submit" value="Submit"></td>
+				</tr>
+			</table>
+			</fieldset>
+		</form>
+
+		@foreach($errors->all() as $err)
+			{{$err}} <br>
+		@endforeach
+</body>
+</html>
---
+++ Final_lab_xm/resources/views/home/index.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Home page</title>
+</head>
+<body>
+	<h1>Welcome home! {{$username}}</h1>
+	<a href="{{route('home.create')}}">Create New Student</a> |
+	<a href="{{route('home.stdlist')}}">View Student List</a> |
+	<a href="/logout">logout</a>
+	<br>
+
+</body>
+</html>
